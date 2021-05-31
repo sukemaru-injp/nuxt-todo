d3b8156590160ab742d91ab10630cf4c534a5e2b
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-05-31 13:40:15 +0900

    swiper

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,57 @@
 <template>
-  <div class="container">
-    <h1>HOME</h1>
+  <div id="container">
+    <section class="sec">
+      <div class="hero">
+        <client-only>
+          <swiper :options="swiperOption">
+            <swiper-slide>
+              <img src="~/assets/images/IMG_4243.png" class="swiper-slide">
+              <div class="hero__title">Travel ToDo</div>
+            </swiper-slide>
+            <swiper-slide>
+              <img src="~/assets/images/IMG_4090.png" class="swiper-slide">
+              <div class="hero__title">あなただけの旅行計画</div>
+            </swiper-slide>
+            <swiper-slide>
+              <img src="~/assets/images/IMG_0735.png" class="swiper-slide">
+              <div class="hero__title">Enjoy Holiday</div>
+            </swiper-slide>
+          </swiper>
+        </client-only>
+      </div>
+      <hr>
+      <div class="content">
+        <router-link to="/todos" class="btn float">ToDoリストを作る</router-link>
+        <router-link to="/todos" class="btn float">ログイン</router-link>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      swiperOption: {
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        effect: 'coverFlow',
+        centeredSlides: true,
+        slidesPerView: 1,
+        loop: true,
+        speed: 1000,
+        grabCursor: true,
+        breakpoints: {
+          600: {
+            slidesPerView: 2,
+          }
+        }
+      },
+    }
+  },
+}
 </script>
 
 <style>
