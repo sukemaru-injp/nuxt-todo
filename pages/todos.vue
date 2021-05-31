77a5161d2e51c47d5e17189dc531ae0b4ed4b8e1
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-05-31 12:49:09 +0900

    Initial commit

---
+++ pages/todos.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="container">
+    <div class="form">
+      <form v-on:submit.prevent="add">
+        <input v-model="name">
+        <button>Add</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      name: '',
+      done: false
+    }
+  },
+  created: function() {
+    this.$store.dispatch('todos/init')
+  },
+  methods: {
+    add() {
+      this.$store.dispatch('todos/add', this.name)
+    }
+  }
+}
+</script>
