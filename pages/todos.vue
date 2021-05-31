31820afb66f30cc6aee6b0b68bab60475b16af05
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-05-31 15:18:51 +0900

    delete

--- pages/todos.vue
+++ pages/todos.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="container">
     <div class="todos">
-      <p>{{ todos }}</p>
+      <ul class="todos_list">
+        <li v-for="todo in todos" :key="todo.id">
+          {{ todo.name }}
+          {{ todo.created }}
+          <button @click="remove(todo.id)">DEL</button>
+        </li>
+      </ul>
     </div>
     <div class="form">
       <form v-on:submit.prevent="add">
-        <input v-model="name">
-        <button>Add</button>
+        <input v-model="name" class="input">
+        <button class="submit">Add</button>
       </form>
     </div>
   </div>
@@ -27,6 +33,9 @@ export default {
     add() {
       this.$store.dispatch('todos/add', this.name)
       this.name = ''
+    },
+    remove(id) {
+      this.$store.dispatch('todos/remove', id)
     }
   },
   computed: {
