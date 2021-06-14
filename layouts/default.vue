91d83ee93d38af0ddd2125d5e188732a8fd46c81
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-06-14 15:18:42 +0900

    nuv create

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
+  <div id="global-container">
     <Header></Header>
     <Nuxt />
+    <nav class="mobile">
+      <ul class="mobile__main">
+        <li class="mobile__item">
+          <a href="#">新規登録</a>
+        </li>
+        <li class="mobile__item">
+          <a href="#">ログイン</a>
+        </li>
+        <li class="mobile__item">
+          <a href="#">Our Service</a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
