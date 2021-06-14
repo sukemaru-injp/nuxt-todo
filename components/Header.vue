268bd041af80d6c2e01c95e3fd37581313172cca
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-06-14 14:48:38 +0900

    header component

---
+++ components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+  <header class="header">
+    <div class="header-left">
+      <router-link to="/" class="title">TRAVEL TODO</router-link>
+    </div>
+    <div class="header-right">
+      <button class="menu-icon" @click="toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggle() {
+      document.querySelector("body").classList.toggle("menu-open")
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  background-color: aquamarine;
+  height: 60px;
+  padding-top: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.title {
+  padding: 10px;
+}
+
+.menu-icon {
+  background-color: transparent;
+  border: none;
+  outline: none !important;
+  margin-right: 10px;
+  margin-top: 6px;
+
+  & > span {
+    display: block;
+    background-color: black;
+    width: 35px;
+    height: 2px;
+    margin-bottom: 10px;
+    transition: all 0.5s;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.menu-open {
+  & .menu-icon {
+    & > span {
+      &:nth-child(1) {
+        transform: translateY(12px) rotate(135deg);
+      }
+
+      &:nth-child(2) {
+        transform: scaleX(0) translateX(20px);
+      }
+
+      &:nth-child(3) {
+        transform: translateY(-12px) rotate(-135deg);
+      }
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,84 +1,18 @@
 <template>
   <div>
-    <header class="header">
-      <div class="header-left">
-        <router-link to="/" class="title">TRAVEL TODO</router-link>
-      </div>
-      <div class="header-right">
-        <button class="menu-icon" @click="toggle">
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-      </div>
-    </header>
+    <Header></Header>
     <Nuxt />
   </div>
 </template>
 
 <script>
+import Header from "~/components/Header.vue"
+
 export default {
-  methods: {
-    toggle() {
-      document.querySelector("body").classList.toggle("menu-open")
-    },
+  components: {
+    Header,
   },
 }
 </script>
 
-<style lang="scss">
-.header {
-  background-color: aquamarine;
-  height: 60px;
-  padding-top: 10px;
-  display: flex;
-  justify-content: space-between;
-}
-
-.title {
-  padding: 10px;
-}
-
-.menu-icon {
-  background-color: transparent;
-  border: none;
-  outline: none !important;
-  margin-right: 10px;
-  margin-top: 6px;
-
-  & > span {
-    display: block;
-    background-color: black;
-    width: 35px;
-    height: 2px;
-    margin-bottom: 10px;
-    transition: all 0.5s;
-
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-
-  &:hover {
-    cursor: pointer;
-  }
-}
-
-.menu-open {
-  & .menu-icon {
-    & > span {
-      &:nth-child(1) {
-        transform: translateY(12px) rotate(135deg);
-      }
-
-      &:nth-child(2) {
-        transform: scaleX(0) translateX(20px);
-      }
-
-      &:nth-child(3) {
-        transform: translateY(-12px) rotate(-135deg);
-      }
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
