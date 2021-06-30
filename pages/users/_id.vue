cabc9acc90ad311230d80aaf843ad1c9b2f7a7d2
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2021-06-30 18:30:30 +0900

    user page

---
+++ pages/users/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>usersID{{ $route.params.id }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  validate({ params }) {
+    return /^\d+$/.test(params.id)
+  },
+}
+</script>
+
+<style scoped></style>
